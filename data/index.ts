7ba931e15c3ff1f56793975cc6b5737a06608072
hoantelseai <108644551+hoantelseai@users.noreply.github.com>
2024-07-30 17:33:53 +0900

    first commit

---
+++ data/index.ts
@@ -0,0 +1,228 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+    //   title: "I prioritize client collaboration, fostering open communication ",
+      title: " 顧客との協力を優先し、オープンなコミュニケーションを促進します。 ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+    //   title: "I'm very flexible with time zone communications",
+      title: "時差を考慮したコミュニケーションに非常に柔軟です。",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "技術スタック。",
+    //   description: "I constantly try to improve",
+      description: "常に改善しようと努力しています：",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+    //   title: "Tech enthusiast with a passion for development.",
+      title: " 技術に熱心で、開発に情熱を持つ人です。",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+    //   title: "Currently building a JS Animation library",
+      title: "現在、JavaScript アニメーションライブラリを開発中です。",
+      description: "The Inside Scoop",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+    //   title: "Do you want to start a project together?",
+      title: "一緒にプロジェクトを始めませんか？",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "BMI-Project",
+      des: "体格指数（Body Mass Index, BMI）を計算し、健康状態を評価するためのプロジェクト。JavaScriptとHTML＆CSSでユーザーが身長と体重を入力すると、BMIを計算し、それぞれの健康に関するアドバイスが表示される。",
+      img: "/BMI-Background.png",
+      // iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+      iconLists: ["/CSS3_logo.svg", "/html-5-logo.svg", "/JavaScript_logo_2.svg"],
+      link: "https://github.com/hoantelseai/BMI-Project.git",
+    },
+    {
+      id: 2,
+      title: "Coffee HomePage - Landing Page Project",
+      des: "コーヒーと食事に関するランディングページプロジェクト。HTML＆CSSとJavaScriptを使用して、落ち着いた雰囲気で、詳細なメニューがあり、訪問者が簡単にアクセスできるウェブページをデザインした。",
+      img: "/HomePage-Background.png",
+      // iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+      iconLists: ["/CSS3_logo.svg", "/html-5-logo.svg", "/JavaScript_logo_2.svg"],
+      link: "https://github.com/hoantelseai/HomePage_PJ.git",
+    },
+    {
+      id: 3,
+      title: "Brainwave Website - Landing Page Project",
+      des: "React.jsとTailwind CSSを使用してモダンなUI/UXウェブサイトを開発した。",
+      img: "/Brainwave-Background.png",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/next.svg", "/Vitejs-logo.svg"],
+      link: "https://github.com/adrianhajdin/ai_saas_app",
+    },
+    {
+      id: 4,
+      title: "Animated Apple Iphone 3D Website",
+      des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+      img: "/p4.svg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+      link: "https://github.com/adrianhajdin/iphone",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "cloudinary",
+      img: "/cloud.svg",
+      nameImg: "/cloudName.svg",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      img: "/app.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "HOSTINGER",
+      img: "/host.svg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "stream",
+      img: "/s.svg",
+      nameImg: "/streamName.svg",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "QA Engineer Intern",
+      desc: "システムテストのテストケースを作成し、単体テスト、総合テストと結合テストを実行しました。",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: "Front-end Web Developer Intern",
+      desc: "HTML、CSS、JavaScriptとReact Nativeを使用してウェブサイトを設計および開発しました。",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp2.svg",
+    },
+    {
+      id: 3,
+      title: "Obtained FE Certificate from Ministry of Economy, Trade and Industry of Japan ",
+      desc: "６か月の学習とトレーニングの後に基本情報技術者試験に合格できました。基本情報技術者試験の勉強をすることでITの基礎知識が身につきました。",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp3.svg",
+    },
+    {
+      id: 4,
+      title: "Lead Frontend Developer",
+      desc: "Developed and maintained user-facing features using modern frontend technologies.",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+      link: "https://github.com/hoantelseai"
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+      link: ""
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+      link:"https://www.linkedin.com/in/hoan-nguyen-phuong-611359244/"
+    },
+  ];
