ec2cc4ee476ae92c3f6b51907f24488280daba35
hoantelseai <108644551+hoantelseai@users.noreply.github.com>
2024-07-31 12:03:00 +0900

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -100,15 +100,15 @@ export const navItems = [
       des: "React.jsとTailwind CSSを使用してモダンなUI/UXウェブサイトを開発した。",
       img: "./Brainwave-Background.png",
       iconLists: ["./re.svg", "./tail.svg", "./ts.svg", "./next.svg", "./Vitejs-logo.svg"],
-      link: "https://github.com/adrianhajdin/ai_saas_app",
+      link: "https://github.com/hoantelseai/BrainwaveWebsite.git",
     },
     {
       id: 4,
-      title: "Animated Apple Iphone 3D Website",
-      des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+      title: "Animated Apple Iphone 3D Website Project",
+      des: "Apple iPhone 15 Proのウェブサイトを再現し、GSAPアニメーションとThree.jsの3Dエフェクトを組み合わせた",
       img: "./p4.svg",
       iconLists: ["./next.svg", "./tail.svg", "./ts.svg", "./three.svg", "./gsap.svg"],
-      link: "https://github.com/adrianhajdin/iphone",
+      link: "https://github.com/hoantelseai/AppleWebsite3D.git",
     },
   ];
   
