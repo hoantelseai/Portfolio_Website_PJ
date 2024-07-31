4e471ae18f8c4cf46227c0c7983ab2372c5ab407
hoantelseai <108644551+hoantelseai@users.noreply.github.com>
2024-07-31 10:32:40 +0900

    deploy

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -38,7 +38,10 @@ const Footer = () => {
                 <div className='flex items-center md:gap-3 gap-6 mb-6'>
                     {socialMedia.map((profile) => (
                         <a key={profile.id} href={profile.link} target = "_blank" rel="noopener noreferrer" className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300'>
-                            <img src={profile.img} alt={profile.id} width={20} height={20} />
+                            <img src={profile.img} 
+                            // alt={profile.id} 
+                            width={20} 
+                            height={20} />
                         </a>
                     ))}
                 </div>
